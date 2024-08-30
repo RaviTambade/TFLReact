bc20d2d148015b3def1a4cf51c7d675db73e4117
Ravi Tambade <ravi.tambade@transflower.in>
2024-08-30 16:44:24 +0530

    updated testapp

--- solutions/ReactBasicsApp/src/Components/JSX/Customer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Customer() {
-  return <h1>Microsoft</h1>;
-}
-
-export default Customer;
--- solutions/ReactBasicsApp/src/Components/JSX/Messanger.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Messanger({ name }) {
-  return <h1>Hello, {name}!</h1>;
-}
-
-export default Messanger;
